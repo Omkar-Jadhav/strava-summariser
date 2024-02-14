74fd15cb6105aeb0930192227ee1a6df02752db8
Omkar-Jadhav <omkar.jadhav00@gmail.com>
2024-02-14 19:01:39 +0530

    commit strava full

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request, jsonify, render_template
-
+import strava
 app = Flask(__name__)
 
 @app.route("/")
@@ -28,3 +28,15 @@ def verify_webhook():
         else:
             return "Invalid verification token", 403
 
+@app.route('/webhook', methods=['POST'])
+def handle_webhook():
+    # data = request.get_json()
+    latest_activity_id = request.args.get('object_id')
+    print("Webhook event received with activity:", latest_activity_id)
+
+    strava.get_latest_activities()
+    # subprocess.run(["python", python_script], check=True, capture_output=True)
+    # stdout = subprocess.check_output(["python", python_script])  # Capture output for logging
+    # print("Python script output:", stdout.decode())
+
+    return jsonify({"message": "EVENT_RECEIVED"}), 200
