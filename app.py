306acabdab8d84e134dbd8cf7181fd9bd35d1621
Omkar-Jadhav <omkar.jadhav00@gmail.com>
2024-02-14 18:49:29 +0530

    adding strava verification

--- app.py
+++ app.py
@@ -7,28 +7,24 @@ app = Flask(__name__)
 def start():
     return {"message": "Hello, world!"}
 
-@app.route("/index")
-def index():
-    return render_template('index.html')
-
 @app.route("/health")
 def health_check():
     return {"status": "healthy"}
 
 
-# VERIFY_TOKEN = "STRAVA"
-# python_script = "strava.py"
+VERIFY_TOKEN = "STRAVA"
+python_script = "strava.py"
 
-# @app.route('/webhook', methods=['GET'])
-# def verify_webhook():
-#     mode = request.args.get('hub.mode')
-#     token = request.args.get('hub.verify_token')
-#     challenge = request.args.get('hub.challenge')
+@app.route('/webhook', methods=['GET'])
+def verify_webhook():
+    mode = request.args.get('hub.mode')
+    token = request.args.get('hub.verify_token')
+    challenge = request.args.get('hub.challenge')
 
-#     if mode and token:
-#         if mode == 'subscribe' and token == VERIFY_TOKEN:
-#             print("Webhook verified")
-#             return jsonify({"hub.challenge": challenge})
-#         else:
-#             return "Invalid verification token", 403
+    if mode and token:
+        if mode == 'subscribe' and token == VERIFY_TOKEN:
+            print("Webhook verified")
+            return jsonify({"hub.challenge": challenge})
+        else:
+            return "Invalid verification token", 403
 
